91fe95d579914792230db249b3b9d4864c924d0b
Ægir Örn Símonarson <agirorn@gmail.com>
2024-10-30 15:28:13 +0000

    Feature: Generate multiple UUIDs
    Added command line argument so the number of UUIDs to generate can be
    configured from the command line

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,20 @@
 use uuid::Uuid;
 
+use clap::Parser;
+
+/// A UUID generator
+#[derive(Parser, Debug)]
+#[command(version, about, long_about = None)]
+struct Args {
+    /// Number of UUIDs to generate
+    #[arg(short, long, default_value_t = 1)]
+    count: u64,
+}
+
 fn main() {
-    let id = Uuid::new_v4();
-    println!("{}", id);
+    let args = Args::parse();
+
+    for _ in 0..args.count {
+        println!("{}", Uuid::new_v4());
+    }
 }
